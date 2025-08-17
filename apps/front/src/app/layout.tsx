de4ebb5019b9e9ed7e8c0e7a76653563cdd92a8d
Mohamed Tahiri <moha.tah@icloud.com>
2025-08-17 20:42:23 +0200

    feat(front): setup dark mode with next-themes
    - Added shadcn/ui components: dropdown menu and button.
    - Integrated theme toggling functionality using next-themes.
    - Updated Prettier and ESLint configurations for improved code formatting.
    - Enhanced global styles and layout structure for better responsiveness.

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -1,40 +1,41 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { ProvidersWrapper } from '@/components/providers/providers-wrapper'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "Mohamed Tahiri",
+  title: 'Mohamed Tahiri',
   description: "Mohamed Tahiri's portfolio",
   icons: {
     icon: [
-      { url: "/favicon-light.ico", media: "(prefers-color-scheme: light)" },
-      { url: "/favicon-dark.ico", media: "(prefers-color-scheme: dark)" },
-    ],
-  },
-};
+      { url: '/favicon-light.ico', media: '(prefers-color-scheme: light)' },
+      { url: '/favicon-dark.ico', media: '(prefers-color-scheme: dark)' }
+    ]
+  }
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ProvidersWrapper>{children}</ProvidersWrapper>
       </body>
     </html>
-  );
+  )
 }
