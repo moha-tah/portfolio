1f4d3b9e90bf858bcb5ef116e6abca5e13a32782
Mohamed Tahiri <moha.tah@icloud.com>
2025-08-25 02:07:54 +0200

    replace comment

--- apps/front/src/components/skills/skills.tsx
+++ apps/front/src/components/skills/skills.tsx
@@ -14,7 +14,7 @@ export function Skills() {
 
   return (
     <ScrollAnimatedSection
-      id="projects" // TODO: replace by "skills"
+      id="projects" // TODO: change to "skills"
       className="w-full pt-8"
       staggerChildren={0.1}
       delayChildren={0.2}
