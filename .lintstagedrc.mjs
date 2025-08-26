a161d32ec760b39c8b38541cfac0d3f9495ebce2
Mohamed Tahiri <moha.tah@icloud.com>
2025-08-26 21:39:05 +0200

    feat(back): enhance backend structure with Swagger integration and linting improvements
    - Added Swagger plugin configuration for better API documentation.
    - Updated linting rules to include backend TypeScript and JavaScript files.
    - Removed obsolete test file for AppController to streamline the codebase.
    - Annotated AppController with Swagger tags for improved API clarity.

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,4 +3,7 @@ export default {
   "apps/front/**/*.{ts,tsx}": (files) => [
     `pnpm lint:fix --filter=front -- --file ${files.join(" ")}`,
   ],
+  "apps/back/**/*.{ts,js}": (files) => [
+    `pnpm lint:fix --filter=back -- ${files.join(" ")}`,
+  ],
 };
