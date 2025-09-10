1b685e03e5102da9ec218450a60ee40660de6eb5
Mohamed Tahiri <moha.tah@icloud.com>
2025-09-11 00:46:07 +0200

    hotfix(front/sitemap): fix sitemap generation with dynamic project URLs
    - Added dynamic project URLs to the sitemap using the staticProjects data.
    - Included a new entry for the projects page in the sitemap with updated change frequency and priority settings.
    - Improved overall structure for better maintainability and scalability of the sitemap.

--- apps/front/src/app/sitemap.ts
+++ apps/front/src/app/sitemap.ts
@@ -1,14 +1,29 @@
 import type { MetadataRoute } from 'next'
 
+import { staticProjects } from '@/components/projects/get-projects'
 import { env } from '@/lib/env'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const projectUrls: MetadataRoute.Sitemap = staticProjects.map(({ slug }) => ({
+    url: `${env.NEXT_PUBLIC_FRONT_URL}/projects/${slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.7
+  }))
+
   return [
     {
       url: env.NEXT_PUBLIC_FRONT_URL,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1
-    }
+    },
+    {
+      url: env.NEXT_PUBLIC_FRONT_URL + '/projects',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8
+    },
+    ...projectUrls
   ]
 }
